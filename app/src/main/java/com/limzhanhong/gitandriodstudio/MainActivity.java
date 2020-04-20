7d7bdb8d1ca561b1960b7d2a8ce3241adcdee604
zhanhong-spec <limhong2001.gmail.com>
2020-04-20 21:38:08 +0800

    First Commit message

--- app/src/main/java/com/limzhanhong/gitandriodstudio/MainActivity.java
+++ app/src/main/java/com/limzhanhong/gitandriodstudio/MainActivity.java
@@ -3,6 +3,7 @@ package com.limzhanhong.gitandriodstudio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("MY TAG","This is also commit msg.");
     }
 }
